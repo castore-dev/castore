d0e3898e688f0dc4f0bcabf82d2c5e3fcb00052a
Charles Géry <charles.gery@gmail.com>
2022-05-27 16:15:49 +0200

    chore: add zodEventType unit tests

---
+++ packages/event-store/src/event/implementations/zod.unit.test.ts
@@ -0,0 +1,117 @@
+import { A } from 'ts-toolbelt';
+import { z } from 'zod';
+
+import { EventTypeDetail } from '../eventType';
+import { ZodEventType } from './zod';
+
+describe('zodEvent implementation', () => {
+  const type = 'SOMETHING_HAPPENED';
+
+  const payloadSchema = z.object({ message: z.string() });
+
+  type Payload = z.infer<typeof payloadSchema>;
+
+  const metadataSchema = z.object({ userEmail: z.string().email() });
+
+  type Metadata = z.infer<typeof metadataSchema>;
+
+  it('has correct properties (no payload, no metadata)', () => {
+    const simpleEventType = new ZodEventType({
+      type,
+    });
+
+    type SimpleEventTypeDetail = EventTypeDetail<typeof simpleEventType>;
+    const assertSimpleEventTypeDetail: A.Equals<
+      SimpleEventTypeDetail,
+      {
+        aggregateId: string;
+        version: number;
+        type: typeof type;
+        timestamp: string;
+      }
+    > = 1;
+    assertSimpleEventTypeDetail;
+
+    expect(Object.keys(simpleEventType)).toHaveLength(3);
+    expect(simpleEventType.type).toStrictEqual(type);
+    expect(simpleEventType.payloadSchema).toStrictEqual(undefined);
+    expect(simpleEventType.metadataSchema).toStrictEqual(undefined);
+  });
+
+  it('has correct properties (with payload, no metadata)', () => {
+    const payloadEventType = new ZodEventType({
+      type,
+      payloadSchema,
+    });
+
+    type PayloadEventTypeDetail = EventTypeDetail<typeof payloadEventType>;
+    const assertPayloadEventTypeDetail: A.Equals<
+      PayloadEventTypeDetail,
+      {
+        aggregateId: string;
+        version: number;
+        type: typeof type;
+        timestamp: string;
+        payload: Payload;
+      }
+    > = 1;
+    assertPayloadEventTypeDetail;
+
+    expect(Object.keys(payloadEventType)).toHaveLength(3);
+    expect(payloadEventType.type).toStrictEqual(type);
+    expect(payloadEventType.payloadSchema).toStrictEqual(payloadSchema);
+    expect(payloadEventType.metadataSchema).toStrictEqual(undefined);
+  });
+
+  it('has correct properties (no payload, with metadata)', () => {
+    const metadataEventType = new ZodEventType({
+      type,
+      metadataSchema,
+    });
+
+    type MetadataEventTypeDetail = EventTypeDetail<typeof metadataEventType>;
+    const assertMetadataEventTypeDetail: A.Equals<
+      MetadataEventTypeDetail,
+      {
+        aggregateId: string;
+        version: number;
+        type: typeof type;
+        timestamp: string;
+        metadata: Metadata;
+      }
+    > = 1;
+    assertMetadataEventTypeDetail;
+
+    expect(Object.keys(metadataEventType)).toHaveLength(3);
+    expect(metadataEventType.type).toStrictEqual(type);
+    expect(metadataEventType.payloadSchema).toStrictEqual(undefined);
+    expect(metadataEventType.metadataSchema).toStrictEqual(metadataSchema);
+  });
+
+  it('has correct properties (with payload, with metadata)', () => {
+    const fullEventType = new ZodEventType({
+      type,
+      payloadSchema,
+      metadataSchema,
+    });
+
+    type FullEventTypeDetail = EventTypeDetail<typeof fullEventType>;
+    const assertFullEventTypeDetail: A.Equals<
+      FullEventTypeDetail,
+      {
+        aggregateId: string;
+        version: number;
+        type: typeof type;
+        timestamp: string;
+        payload: Payload;
+        metadata: Metadata;
+      }
+    > = 1;
+    assertFullEventTypeDetail;
+
+    expect(Object.keys(fullEventType)).toHaveLength(3);
+    expect(fullEventType.type).toStrictEqual(type);
+    expect(fullEventType.payloadSchema).toStrictEqual(payloadSchema);
+    expect(fullEventType.metadataSchema).toStrictEqual(metadataSchema);
+  });
+});
