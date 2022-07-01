425e1fa72913ebc5bd1d3bf9207257906c8aa0b6
Charles Géry <charles.gery@gmail.com>
2022-07-01 16:18:40 +0200

    feat: create event packages

---
+++ packages/json-schema-event/src/jsonSchema.ts
@@ -0,0 +1,44 @@
+import { FromSchema, JSONSchema } from 'json-schema-to-ts';
+
+import { EventDetail, EventType } from '@castore/event-store';
+import type { OmitUndefinableKeys } from '@castore/event-store';
+
+export class JSONSchemaEventType<
+  T extends string = string,
+  // Metadata
+  MS extends JSONSchema | undefined = JSONSchema | undefined,
+  PS extends JSONSchema | undefined = JSONSchema | undefined,
+  $D = OmitUndefinableKeys<{
+    aggregateId: string;
+    version: number;
+    type: T;
+    timestamp: string;
+    payload: PS extends JSONSchema ? FromSchema<PS> : undefined;
+    metadata: MS extends JSONSchema ? FromSchema<MS> : undefined;
+  }>,
+  D extends EventDetail = $D extends EventDetail ? $D : never,
+> implements EventType<T, D>
+{
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  _types: {
+    detail: D;
+  };
+  type: T;
+  payloadSchema?: PS;
+  metadataSchema?: MS;
+
+  constructor({
+    type,
+    payloadSchema,
+    metadataSchema,
+  }: {
+    type: T;
+    payloadSchema?: PS;
+    metadataSchema?: MS;
+  }) {
+    this.type = type;
+    this.payloadSchema = payloadSchema;
+    this.metadataSchema = metadataSchema;
+  }
+}
---
+++ packages/zod-event/src/zodEvent.ts
@@ -0,0 +1,43 @@
+import { z, ZodType } from 'zod';
+
+import { EventDetail, EventType } from '@castore/event-store';
+import type { OmitUndefinableKeys } from '@castore/event-store';
+
+export class ZodEventType<
+  T extends string = string,
+  MS extends ZodType | undefined = ZodType | undefined,
+  PS extends ZodType | undefined = ZodType | undefined,
+  $D = OmitUndefinableKeys<{
+    aggregateId: string;
+    version: number;
+    type: T;
+    timestamp: string;
+    payload: PS extends ZodType ? z.infer<PS> : undefined;
+    metadata: MS extends ZodType ? z.infer<MS> : undefined;
+  }>,
+  D extends EventDetail = $D extends EventDetail ? $D : never,
+> implements EventType<T, D>
+{
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  _types: {
+    detail: D;
+  };
+  type: T;
+  payloadSchema?: PS;
+  metadataSchema?: MS;
+
+  constructor({
+    type,
+    payloadSchema,
+    metadataSchema,
+  }: {
+    type: T;
+    payloadSchema?: PS;
+    metadataSchema?: MS;
+  }) {
+    this.type = type;
+    this.payloadSchema = payloadSchema;
+    this.metadataSchema = metadataSchema;
+  }
+}
