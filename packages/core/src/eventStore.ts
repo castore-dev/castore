3e1fcd5c42b15de11bc45f2c1b4fc5920dc2b98e
Juliette Fournier <juliettef@theodo.fr>
2022-08-26 16:53:48 +0200

    :label: export event store types

--- packages/core/src/eventStore.ts
+++ packages/core/src/eventStore.ts
@@ -208,8 +208,14 @@ export class EventStore<
   }
 }
 
+export type EventStoreId<E extends EventStore> = E['eventStoreId'];
+
+export type EventStoreEventsTypes<E extends EventStore> = E['eventStoreEvents'];
+
 export type EventStoreEventsDetails<E extends EventStore> =
   E['_types']['details'];
+
+export type EventStoreReducer<E extends EventStore> = E['reduce'];
 
 export type EventStoreAggregate<E extends EventStore> =
   E['_types']['aggregate'];
