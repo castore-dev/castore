b6895524820e59ff8708d14d3aa90b2946486f83
anaisberg <anais.schlienger@lilo.org>
2023-11-10 16:21:48 +0100

    ✨ use dynamo db single table adapter

--- demo/implementation/libs/eventStores/pokemons.ts
+++ demo/implementation/libs/eventStores/pokemons.ts
@@ -1,12 +1,12 @@
 import { pokemonsEventStore as $pokemonsEventStore } from '@castore/demo-blueprint';
-import { LegacyDynamoDBEventStorageAdapter } from '@castore/event-storage-adapter-dynamodb';
+import { DynamoDBSingleTableEventStorageAdapter } from '@castore/event-storage-adapter-dynamodb';
 
 import { dynamoDBClient } from './client';
 
 export const pokemonsEventStore = $pokemonsEventStore;
 
-pokemonsEventStore.eventStorageAdapter = new LegacyDynamoDBEventStorageAdapter({
-  tableName: process.env.POKEMON_EVENTS_TABLE_NAME as string,
-
-  dynamoDBClient,
-});
+pokemonsEventStore.eventStorageAdapter =
+  new DynamoDBSingleTableEventStorageAdapter({
+    tableName: process.env.POKEMON_EVENTS_TABLE_NAME as string,
+    dynamoDBClient,
+  });
